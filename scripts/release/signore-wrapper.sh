1a4db137ededc2a781030ed4a3596c8c7aa3dc61
Ilia Gogotchuri <ilia.gogotchuri0@gmail.com>
2025-09-02 13:08:02 +0400

    Workflow changes, removes nightly and release workflows not used for tofu-exec.
    Signed-off-by: Ilia Gogotchuri <ilia.gogotchuri0@gmail.com>

--- scripts/release/signore-wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Copyright (c) The OpenTofu Authors
-# SPDX-License-Identifier: MPL-2.0
-# Copyright (c) 2023 HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-printf '\n[GNUPG:] SIG_CREATED ' >&${1#--status-fd=}
-signore sign --file /dev/stdin --signer $3 2>/dev/null
